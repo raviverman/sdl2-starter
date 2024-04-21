3ef2a7a64acd311fc0e9a444e3d5b46fa8c36b3b
Ravi Verman <raviverman@hotmail.com>
2024-04-21 18:20:12 +0530

    Starter: Make it bare basic

--- init.c
+++ init.c
@@ -3,14 +3,6 @@
 #include<unistd.h>
 #include<stdio.h>
 
-bool isMouseOver( SDL_Rect rect, int mouse_x, int mouse_y ) {
-   if ( mouse_x >= rect.x && mouse_x <= rect.x + rect.w &&
-        mouse_y >= rect.y && mouse_y <= rect.y + rect.h ) {
-      return true;
-   }
-   return false;
-}
-
 int main() {
 
    SDL_Window* window = NULL;
@@ -23,6 +15,7 @@ int main() {
    printf("Window: %p, Render: %p\n", window, renderer );
 
 
+   /* Draw a rectangle */
    SDL_SetRenderDrawColor( renderer, 255, 255, 255, 255 );
    SDL_Rect rect;
    rect.w = 100;
@@ -30,51 +23,17 @@ int main() {
    rect.x = 20;
    rect.y = 20;
 
-   SDL_Rect rect2;
-   rect2.w = 100;
-   rect2.h = 100;
-   rect2.x = 140;
-   rect2.y = 20;
-
    SDL_RenderDrawRect( renderer, &rect );
-   SDL_RenderDrawRect( renderer, &rect2 );
-
    SDL_RenderPresent( renderer );
 
-   int count = 0;
    int is_running = true;
-   int mouse_x, mouse_y;
    SDL_Event event;
    while (is_running) {
       while (SDL_PollEvent(&event)) {
           if (event.type == SDL_QUIT) {
               is_running = false;
-          } else if ( event.type == SDL_MOUSEMOTION ) {
-            SDL_GetMouseState(&mouse_x, &mouse_y);
-            printf("Mouse Motion (%d, %d) CPU Cnt: %d\n", mouse_x, mouse_y,
-                   SDL_GetCPUCount());
          }
       }
-
-      /*clear canvas*/
-      SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
-      SDL_RenderClear( renderer );
-      /* render content */
-      if (isMouseOver(rect, mouse_x, mouse_y)) {
-         SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
-      } else {
-         SDL_SetRenderDrawColor( renderer, 255, 255, 255, 255 );
-      }
-      SDL_RenderDrawRect( renderer, &rect );
-      if (isMouseOver(rect2, mouse_x, mouse_y)) {
-         SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
-      } else {
-         SDL_SetRenderDrawColor( renderer, 255, 255, 255, 255 );
-      }
-      SDL_RenderDrawRect( renderer, &rect2 );
-      /* show content */
-      SDL_RenderPresent( renderer );
-
       SDL_Delay(10);
    }
 
